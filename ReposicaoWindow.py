f2ddd0eb150bd4910ce03074116396bcf8312bdc
Verner Rafael <vernerrrferreira@gmail.com>
2026-05-06 23:45:01 -0300

    Produto_Final

--- ReposicaoWindow.py
+++ ReposicaoWindow.py
@@ -2,4 +2,4 @@ import Window
 
 class ReposicaoWindow(Window.Window):
     def __init__(self, stack):
-        super().__init__(stack)
+        super().__init__(stack, self.resource_path("./Planilhas/reposicao.xlsx"))
